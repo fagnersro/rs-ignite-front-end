e93ddb818d6b1419c058eb95888e05d97447ab51
Fagenrsro <fagner.hg40@gmail.com>
2023-03-12 11:51:29 -0300

    Application structure - Component: Comment

--- project-1/01-fundamentos-react-js/src/components/Comment.jsx
+++ project-1/01-fundamentos-react-js/src/components/Comment.jsx
@@ -1,5 +1,37 @@
+import { ThumbsUp, Trash } from "phosphor-react";
 import styles from "./Comment.module.css";
 
 export function Comment() {
-  return <div className={styles.comment}></div>;
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/fagnersro.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Fagner Henrique</strong>
+
+              <time title="11 de mar 20:16" dataTime="2022-03-11 20:16:28">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, Parabéns!! </p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
 }
