ecf69c98ad3299ce8877ca7cd96fa9073da51a78
Fagenrsro <fagner.hg40@gmail.com>
2023-03-12 22:28:20 -0300

    Improving application - TypeScript-React

---
+++ project-1/01-fundamentos-react-js-ts/src/components/Posts.tsx
@@ -0,0 +1,131 @@
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from "react";
+import { format, formatDistanceToNow } from "date-fns";
+import ptBr from "date-fns/locale/pt-BR";
+
+import { Comment } from "./Comment";
+import { Avatar } from "./Avatar";
+
+import styles from "./Posts.module.css";
+
+interface Author {
+  name: string;
+  role: string;
+  avatarUrl: string;
+}
+
+interface PostProps {
+  author: Author;
+  publishedAt: Date;
+  content: Content[];
+}
+
+interface Content {
+  type: "paragraph" | "link";
+  content: string;
+}
+
+export function Post({ author, publishedAt, content }: PostProps) {
+  const [comments, setComments] = useState(["Post muito bacana, hein?!"]);
+  const [newCommentText, setNewCommentText] = useState("");
+
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    { locale: ptBr }
+  );
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBr,
+    addSuffix: true,
+  });
+
+  function handleCreateNewComment(event: FormEvent) {
+    event.preventDefault();
+
+    setComments((s) => [...s, newCommentText]);
+    setNewCommentText("");
+  }
+
+  function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
+    event.target.setCustomValidity("");
+    setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
+    event.target.setCustomValidity("Esse campo é obrigatório");
+  }
+
+  function deleteComment(commentToDelete: String) {
+    const commentsWithoutDeleteOne = comments.filter((comment) => {
+      return comment !== commentToDelete;
+    });
+
+    setComments(commentsWithoutDeleteOne);
+  }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
+
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src={author.avatarUrl} />
+          <div className={styles.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
+        </div>
+
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        {content.map((line) => {
+          if (line.type === "paragraph") {
+            return <p key={line.content}>{line.content}</p>;
+          } else if (line.type === "link") {
+            return (
+              <p key={line.content}>
+                <a href="#">{line.content}</a>
+              </p>
+            );
+          }
+        })}
+      </div>
+
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
+        <strong>Deixe seeu feedback</strong>
+
+        <textarea
+          onChange={handleNewCommentChange}
+          placeholder="Deixe um comentário"
+          value={newCommentText}
+          onInvalid={handleNewCommentInvalid}
+          required
+        />
+        <footer>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        {comments.map((comment) => {
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          );
+        })}
+      </div>
+    </article>
+  );
+}
---
+++ project-1/01-fundamentos-react-js-ts/src/components/Sidebar.tsx
@@ -0,0 +1,28 @@
+import styles from "./Sidebar.module.css";
+import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8RGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/fagnersro.png" />
+
+        <strong>Fagner Henrique</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar Perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
