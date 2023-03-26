eadab0f3ef3ff8acc952450e1389fef89065d257
fagnersro <fagner.hg40@gmail.com>
2023-03-26 18:41:00 -0300

    Connection with the API - Formatting values

--- project-3/03-dt-money/src/components/Summary/index.tsx
+++ project-3/03-dt-money/src/components/Summary/index.tsx
@@ -2,6 +2,7 @@ import { SummaryContainer, SummeryCard } from './styles'
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
 import { useContext } from 'react'
 import { TransactionsContext } from '../../contexts/TransactionsContext'
+import { priceFormatter } from '../../utils/formatter'
 export function Summary() {
   const { transactions } = useContext(TransactionsContext)
   const summary = transactions.reduce(
@@ -30,7 +31,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>{summary.income}</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </SummeryCard>
 
       <SummeryCard>
@@ -39,7 +40,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>{summary.outcome}</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummeryCard>
 
       <SummeryCard variant="green">
@@ -48,7 +49,7 @@ export function Summary() {
           <CurrencyDollar size={32} color="#FFF" />
         </header>
 
-        <strong>{summary.total}</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </SummeryCard>
     </SummaryContainer>
   )
