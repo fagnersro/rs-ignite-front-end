c3014ab8998db333ec61dd5a84b438f6cf595813
fagnersro <fagner.hg40@gmail.com>
2023-03-28 12:23:32 -0300

    Visual structure - Application header

--- project-4.1/04-ignite-shop/src/pages/_app.tsx
+++ project-4.1/04-ignite-shop/src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import { globalStyles } from '@/styles/global'
+import { Container, Header } from '@/styles/pages/app'
 import {Roboto} from '@next/font/google'
 import type { AppProps } from 'next/app'
-
+import logoImg from '../assets/Logo.svg'
 const roboto = Roboto({
   weight:['400','700'],
   style:['normal'],
@@ -9,10 +10,14 @@ const roboto = Roboto({
 })
 
 globalStyles()
+
 export default function App({ Component, pageProps }: AppProps) {
 
   return  (
-    <>
+    <Container>
+   <Header>
+    <img src={logoImg.src} alt="" />
+   </Header>
     <style jsx global>{
         `
           html {
@@ -21,6 +26,6 @@ export default function App({ Component, pageProps }: AppProps) {
         `
       }</style>
       <Component {...pageProps} />
-    </>
+    </Container>
   ) 
 }
