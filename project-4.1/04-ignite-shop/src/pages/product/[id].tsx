f95389e9805d3414ec63cb4321264422411bcd44
fagnersro <fagner.hg40@gmail.com>
2023-03-29 17:24:02 -0300

    Product & Checkout - Product page structure

--- project-4.1/04-ignite-shop/src/pages/product/[id].tsx
+++ project-4.1/04-ignite-shop/src/pages/product/[id].tsx
@@ -1,9 +1,25 @@
+import { ImageContainer, ProductContainer, ProductDetails } from '@/styles/pages/product'
 import { useRouter } from 'next/router'
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut ullam architecto nihil nostrum obcaecati tempore asperiores tenetur alias, quia hic similique molestiae aperiam tempora minus enim qui odit ex delectus!</p>
+      
+        <button>
+          Comprar agora 
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
