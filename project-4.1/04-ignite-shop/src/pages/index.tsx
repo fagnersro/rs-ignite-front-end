31ffbc4dae4231d2e2ad8529f66e7487fb76a6b6
fagnersro <fagner.hg40@gmail.com>
2023-03-28 23:42:21 -0300

    Stripe Integration - Data fetching on Next

--- project-4.1/04-ignite-shop/src/pages/index.tsx
+++ project-4.1/04-ignite-shop/src/pages/index.tsx
@@ -55,3 +55,13 @@ export default function Home() {
     </HomeContainer>
   )
 }
+
+export const getServerSideProps = async () => {
+  await new Promise(resolve => setTimeout(resolve, 2000))
+
+  return {
+    props: {
+      list: [1, 2, 3]
+    }
+  }
+}
