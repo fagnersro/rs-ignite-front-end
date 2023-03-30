c5f14aeb9bb23d334465a57126382ff418ce5aa6
fagnersro <fagner.hg40@gmail.com>
2023-03-29 23:19:01 -0300

    Product & Checkout - Buy Button

--- project-4.1/04-ignite-shop/src/pages/product/[id].tsx
+++ project-4.1/04-ignite-shop/src/pages/product/[id].tsx
@@ -1,7 +1,9 @@
 import { stripe } from '@/lib/stripe'
 import { ImageContainer, ProductContainer, ProductDetails } from '@/styles/pages/product'
-import { GetServerSideProps, GetStaticPaths, GetStaticProps } from 'next'
+import axios from 'axios'
+import {  GetStaticPaths, GetStaticProps } from 'next'
 import Image from 'next/image'
+import { useState } from 'react'
 import Stripe from 'stripe'
 
 
@@ -17,8 +19,24 @@ interface ProductProps {
 }
 
 export default function Product({ product }: ProductProps) {
-  function handleBuyProduct() {
-    console.log(product.defaultPriceId)
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false)
+   async function handleBuyProduct() {
+    try {
+      setIsCreatingCheckoutSession(true)
+
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId,
+      })
+
+      const { checkoutUrl } = response.data;
+      window.location.href = checkoutUrl
+
+    } catch (err) {
+      // Conectar com uma ferramenta de observabilidade (Datadog) / (Sentry)
+      setIsCreatingCheckoutSession(false)
+
+      alert('Falha ao redirecionar ao checkout!')
+    }
   }
 
   return (
@@ -33,7 +51,7 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
       
-        <button onClick={handleBuyProduct}>
+        <button  disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
           Comprar agora 
         </button>
       </ProductDetails>
