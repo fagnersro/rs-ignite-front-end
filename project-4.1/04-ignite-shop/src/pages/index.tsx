b75020226f6bd701fcb7244e4064e0af535bf660
fagnersro <fagner.hg40@gmail.com>
2023-03-30 01:22:14 -0300

    Product & Checkout - Working with SEO

--- project-4.1/04-ignite-shop/src/pages/index.tsx
+++ project-4.1/04-ignite-shop/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import { HomeContainer, Product } from "../styles/pages/home"
+import Head from "next/head"
 import { useKeenSlider } from 'keen-slider/react'
 import { GetStaticProps } from "next"
 import Link from "next/link"
@@ -29,22 +30,29 @@ export default function Home({ products }: HomeProps) {
   })
 
   return (
-    <HomeContainer ref={sliderRef} className="keen-slider">
-      {products.map((product) => {
-        return (
-          <Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
-            <Product className="keen-slider__slide" >
-              <Image src={product.imageUrl} width={520} height={480} alt="" />
+    <>
+      <Head>
+        <title>Ignite Shop</title>
+      </Head>
+      
 
-              <footer>
-                <strong>{product.name}</strong>
-                <span>{product.price}</span>
-              </footer>
-            </Product>
-          </Link>
-        )
-      })}
-    </HomeContainer>
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        {products.map((product) => {
+          return (
+            <Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
+              <Product className="keen-slider__slide" >
+                <Image src={product.imageUrl} width={520} height={480} alt="" />
+
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>
+          )
+        })}
+      </HomeContainer>
+    </>
   )
 }
 
--- project-4.1/04-ignite-shop/src/pages/success.tsx
+++ project-4.1/04-ignite-shop/src/pages/success.tsx
@@ -1,6 +1,7 @@
 import { stripe } from "@/lib/stripe";
 import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
 import { GetServerSideProps } from "next";
+import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import Stripe from "stripe";
@@ -15,19 +16,27 @@ interface SuccessProps {
 
 export default function Success({ customerName, product}: SuccessProps) {
   return (
-    <SuccessContainer>
-      <h1>Compra efetuada!</h1>
-      <ImageContainer>
-        <Image src={product.imageUrl} width={120} height={110} alt=""/>
-      </ImageContainer>
-      <p>
-        Uhuull <strong>{customerName}</strong>, sua <strong>{product.name}</strong> já está a caminho de casa.
-      </p>
+    <>
+      <Head>
+        <title>Compra Efetuada | Ignite Shop</title>
+        
+        <meta name="robots" content="noindex" />
+      </Head>
 
-      <Link href="/">
-        Voltar ao catálogo
-      </Link>
-    </SuccessContainer>
+      <SuccessContainer>
+        <h1>Compra efetuada!</h1>
+        <ImageContainer>
+          <Image src={product.imageUrl} width={120} height={110} alt=""/>
+        </ImageContainer>
+        <p>
+          Uhuull <strong>{customerName}</strong>, sua <strong>{product.name}</strong> já está a caminho de casa.
+        </p>
+
+        <Link href="/">
+          Voltar ao catálogo
+        </Link>
+      </SuccessContainer>
+    </>
   )
 }
 
