369de6e657fb664e3c95ded361c9a33a7063caa8
fagnersro <fagner.hg40@gmail.com>
2023-04-04 00:56:58 -0300

    Storybook improvements - Controls on components

--- project-5/05-design-system/packages/docs/src/stories/Avatar.stories.tsx
+++ project-5/05-design-system/packages/docs/src/stories/Avatar.stories.tsx
@@ -9,6 +9,13 @@ export default {
     src: 'https://github.com/fagnersro.png',
     alt: 'Fagner Henrique',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- project-5/05-design-system/packages/docs/src/stories/Text.stories.tsx
+++ project-5/05-design-system/packages/docs/src/stories/Text.stories.tsx
@@ -6,8 +6,31 @@ export default {
   component: Text,
 
   args: {
+    size: 'md',
     children:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi tempora pariatur optio error qui, ipsum nemo mollitia dolores! Recusandae velit fuga soluta id cum veritatis minus nam mollitia ipsum doloremque!',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
